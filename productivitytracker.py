cd384230e6023fe76ffa714b8712c165e4bd1a0b
bcript <139629525+bcript@users.noreply.github.com>
2023-12-18 16:04:01 +0800

    Update productivitytracker.py
    There are several issues with this version.
    1. You can key in the file name twice ("Notion.exe", "Notion.exe") etc.
    2. Doesn't run the program after the time is given. Goes to "file path" prompt instead.

--- productivitytracker.py
+++ productivitytracker.py
@@ -1,27 +1,92 @@
-import time
-import psutil
+import psutil #pass test
+import time #pass test
+from plyer import notification #pass test
+import os
 import pygame
-from plyer import notification
-
-def detect_game():
-    pygame.mixer.init()
-    sound_playing = False
-
-    # List of processes associated with games
-    game_processes = ["steam.exe"]
-
+app_list = [ ]
+user_time = int("60")
+user_input_path = ""
+#add one on top: 
+#1. when they run the program, it will ask do you have any programs you want to add Y -> setting_up() N -> run program? N -> close Y -> start detector detects true -> start clock
+def main():
     while True:
-        # Check if a game is running
-        game_running = any(process.name().lower() in game_processes for process in psutil.process_iter(['name']))
+        start_choice = input("Do you have any programs to add? (Y/N): ")
+        if start_choice == 'Y':
+            setting_up_programs()
+        elif start_choice == 'N':
+            prompt_tostart()
+        else:
+            print("Enter a valid input.")
 
-        if game_running and not sound_playing:
-            start_timer()
-            sound_playing = True
-        elif not game_running and sound_playing:
-            stop_timer()
-            sound_playing = False
+def setting_up_programs(): #works
+    while True:
+        add_programs = input("Programs to be added to watchlist (KEY 'D' when done): ")
+        if add_programs.endswith(".exe"):
+            app_list.append(add_programs)
+            print(app_list)
+        elif add_programs == "D":
+            print(app_list)
+            choose_song()
+            return False
+        else:
+            print("Error")
+#--------------------------------------------------------------------------------------------------------
+def is_valid_file_path(path):
+    return os.path.isfile(path)
 
-        time.sleep(1 * 60)  # Check every 5 minutes
+#lets the user choose the song they want to play
+def choose_song():
+    while True:
+        #Get user input for a file path
+        user_input_path = input("Enter a file path: ")
+
+        # Check if the input is a valid file path
+        if is_valid_file_path(user_input_path):
+            print(f"{user_input_path} is a valid file path.")
+            prompt_tostart()
+            return False
+        else:
+            print(f"{user_input_path} is not a valid file path.")
+            return True
+        
+def prompt_tostart(): #prompts whether they want to start the program (MAIN PAGE)
+    while True:
+        start = input("Are you ready to run the program? (Y/N): ")
+        if start == "Y":
+            user_time = input('How long before each bell rings? (give your number in seconds): ')
+            if isinstance(user_time, int):
+                print("Valid number.")
+                start_timer()
+            else:
+                break
+
+            #TBD
+        elif start == "N":
+            print("goodbye")
+            break
+        else:
+            print("goodbye")
+
+#checks if process is running
+def is_process_running(process_name):
+    for process in psutil.process_iter(['pid', 'name']):
+        if process.info['name'] == process_name:
+            return True
+    return False
+
+# List of applications to check
+
+# Check if any of the applications is running
+for app_name in app_list:
+    if is_process_running(app_name):
+        print(f"{app_name} is running.")
+
+    else:
+        print(f"{app_name} is not running.")
+
+def stop_timer():
+    # Stop the sound and reset the timer
+    pygame.mixer.music.stop()
 
 def start_timer():
     start_time = time.time()
@@ -29,20 +94,11 @@ def start_timer():
     while True:
         elapsed_time = time.time() - start_time
 
-        if elapsed_time >= 1 * 60:  # 1 hour
+        if elapsed_time >= user_time:  # 1 hour
             play_sound()
             send_notification()
 
-        time.sleep(1 * 60)  # Check every 5 minutes
-
-def stop_timer():
-    # Stop the sound and reset the timer
-    pygame.mixer.music.stop()
-
-def play_sound():
-    # Play a sound (replace 'alarm_clock.mp3' with the path to your sound file)
-    pygame.mixer.music.load('C:\\Users\\Basil\\Downloads\\alarm_clock.mp3')
-    pygame.mixer.music.play()
+        time.sleep(1 * 60) 
 
 def send_notification():
     # Send a desktop notification
@@ -51,5 +107,10 @@ def send_notification():
         message='You have been gaming for 1 hour. Take a break!',
     )
 
+def play_sound():
+    # Play a sound
+    pygame.mixer.music.load(user_input_path)
+    pygame.mixer.music.play()
+    
 if __name__ == "__main__":
-    detect_game()
+    main()
